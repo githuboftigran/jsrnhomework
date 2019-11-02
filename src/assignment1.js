ddebc614d0dacdc0d458368248045a0913d49f12
mikayel-atayan <mikayel.ayatan@picsart.com>
2019-11-02 16:17:29 +0400

    invalid center figures filtering added

--- src/assignment1.js
+++ src/assignment1.js
@@ -12,7 +12,8 @@ window.onload = function () {
         '|shape:triangle/center:340,389/length:180/velocity:7,1/color:#19a6' +
         '|shape:circle/center:500,500/radius:70/velocity:-4,-6/color:#b1ba' +
         '|shape:rect/center:200,128/width:100/height:84/velocity:2,-4/color:#344a' +
-        '|shape:triangle/center:340,389/length:150/velocity:7,10/color:#49f6');
+        '|shape:triangle/center:340,389/length:150/velocity:7,10/color:#49f6')
+        .filter((value) => value.containsX(width) && value.containsY(height));
     window.setInterval(function () {
         context.clearRect(0, 0, canvas.width, canvas.height);
         figures.forEach((value) => {
@@ -34,7 +35,19 @@ function parseFigure(string) {
         velocity: [2, 2],
         changeDirection: function (index) {
             this.velocity[index] = -this.velocity[index];
-        }
+        },
+        checkIfContainsAndChangeDirection: function (width, height) {
+            let contains = true;
+            if (!this.containsX(width)) {
+                contains = false;
+                this.changeDirection(0);
+            }
+            if (!this.containsY(height)) {
+                contains = false;
+                this.changeDirection(1);
+            }
+            return contains;
+        },
     };
     string.split('/').forEach((value) => {
         const pair = value.split(':');
@@ -64,13 +77,11 @@ function addSpecifics(figure) {
                 context.closePath();
                 context.fill();
             };
-            figure.checkIfContainsAndChangeDirection = function (width, height) {
-                if (this.center[0] - this.length / 2 <= 0 || this.center[0] + this.length / 2 >= width) {
-                    this.changeDirection(0);
-                }
-                if (this.center[1] - 2 * sin60 * this.length / 3 <= 0 || this.center[1] + sin60 * this.length / 3 >= height) {
-                    this.changeDirection(1);
-                }
+            figure.containsX = function (width) {
+                return !(this.center[0] - this.length / 2 <= 0 || this.center[0] + this.length / 2 >= width);
+            };
+            figure.containsY = function (height) {
+                return !(this.center[1] - 2 * sin60 * this.length / 3 <= 0 || this.center[1] + sin60 * this.length / 3 >= height);
             };
             break;
         case 'rect':
@@ -84,13 +95,11 @@ function addSpecifics(figure) {
                 context.fillStyle = this.color;
                 context.fillRect(this.center[0] - (this.width / 2), this.center[1] - (this.height / 2), this.width, this.height);
             };
-            figure.checkIfContainsAndChangeDirection = function (width, height) {
-                if (this.center[0] - this.width / 2 <= 0 || this.center[0] + this.width / 2 >= width) {
-                    this.changeDirection(0);
-                }
-                if (this.center[1] - this.height / 2 <= 0 || this.center[1] + this.height / 2 >= height) {
-                    this.changeDirection(1);
-                }
+            figure.containsX = function (width) {
+                return !(this.center[0] - this.width / 2 <= 0 || this.center[0] + this.width / 2 >= width);
+            };
+            figure.containsY = function (height) {
+                return !(this.center[1] - this.height / 2 <= 0 || this.center[1] + this.height / 2 >= height);
             };
             break;
         case 'circle':
@@ -103,13 +112,11 @@ function addSpecifics(figure) {
                 context.arc(this.center[0], this.center[1], this.radius, 0, Math.PI * 2);
                 context.fill();
             };
-            figure.checkIfContainsAndChangeDirection = function (width, height) {
-                if (this.center[0] - this.radius <= 0 || this.center[0] + this.radius >= width) {
-                    this.changeDirection(0);
-                }
-                if (this.center[1] - this.radius <= 0 || this.center[1] + this.radius >= height) {
-                    this.changeDirection(1);
-                }
+            figure.containsX = function (width) {
+                return !(this.center[0] - this.radius <= 0 || this.center[0] + this.radius >= width);
+            };
+            figure.containsY = function (height) {
+                return !(this.center[1] - this.radius <= 0 || this.center[1] + this.radius >= height);
             };
             break;
     }
